5ea3b79e81e7d37cba5262090a5a7673e2eb8df7
Amrit Aujla <amritpalaujla204@gmail.com>
2025-05-27 21:52:32 -0700

    update so html tags are working as intended

--- my-app/src/Components/Previewer.js
+++ my-app/src/Components/Previewer.js
@@ -2,7 +2,8 @@ import Editor from "./Editor";
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 function previewer(props) {
-  return <div id="previewer">{marked.parse(props.input)}</div>;
+  document.getElementById("previewer").innerHTML = marked.parse(props.input);
+  return <div id="previewer"></div>;
 }
 
 export default previewer;
