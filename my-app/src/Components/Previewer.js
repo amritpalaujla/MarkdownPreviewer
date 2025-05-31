60b42a18587df305f936124bd57fb909850de47d
Amrit Aujla <amritpalaujla204@gmail.com>
2025-05-31 12:03:19 -0700

    sanitized from attacks

--- my-app/src/Components/Previewer.js
+++ my-app/src/Components/Previewer.js
@@ -1,11 +1,14 @@
 import Editor from "./Editor";
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import DOMPurify from "dompurify";
 
 function previewer(props) {
   return (
     <div
       id="previewer"
-      dangerouslySetInnerHTML={{ __html: marked.parse(props.input) }}
+      dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(marked.parse(props.input)),
+      }}
     ></div>
   );
 }
